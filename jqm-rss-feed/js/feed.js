4a9f78d82612f2c6cebc2059d28721f4908e1cf4
leoz2011 <leoz@yahoo.com>
2014-03-19 21:19:22 -0400

    Correct style

--- jqm-rss-feed/js/feed.js
+++ jqm-rss-feed/js/feed.js
@@ -1,6 +1,6 @@
 
 function getRSS(url, container, num) {
-    var source = "";
+    var source = '';
     source += document.location.protocol;
     source += '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=';
     source += num;
@@ -11,19 +11,19 @@ function getRSS(url, container, num) {
         url: source,
         dataType: 'json',
         success: function(data) {
-            $.each(data.responseData.feed.entries, function(key, value){
+            $.each(data.responseData.feed.entries, function(key, value) {
                 //console.log(value);
                 
                 var s = '';
                 s += '<li>';
                 s += '<h2>' + value.author + '</h2>';
-                s += '<p><strong>' + (value.title ? value.title : '.' ) + '</strong></p>';
+                s += '<p><strong>' + (value.title ? value.title : '.') + '</strong></p>';
                 s += '<div class="entry_content">' + value.content + '</div>';
                 s += '<p class="ui-li-aside"><strong>' + value.publishedDate + '</strong></p>';
                 s += '</li>';
                 
                 $(container).append(s);
-                $(container).listview("refresh");
+                $(container).listview('refresh');
             });
         }
     });
