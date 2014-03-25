fb0fe5be41a5c7099c77a0cfb9b4b2d27d2307ad
leoz2011 <leoz@yahoo.com>
2014-03-24 23:17:50 -0400

    Add fixed toolbars to RSS sample

--- jqm-rss-feed-complex/js/app.js
+++ jqm-rss-feed-complex/js/app.js
@@ -1,3 +1,6 @@
+$(function() {
+    $( "[data-role='header'], [data-role='footer']" ).toolbar();
+});
 
 $(document).on('pageshow', '#main_page', function() {
     getRSS('http://leoz-net.livejournal.com/data/rss', '#livejournal', 17);
